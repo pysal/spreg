c419a316bda538a42a966a5f27b8a06f3e5b0f13
James Gaboardi <jgaboardi@gmail.com>
2021-01-04 11:07:30 -0500

    change branch main -> master for binder link

--- docs/conf.py
+++ docs/conf.py
@@ -270,7 +270,7 @@ intersphinx_mapping = {
 # This is processed by Jinja2 and inserted before each notebook
 nbsphinx_prolog = r"""
 {% set docname = env.doc2path(env.docname, base='') %}
-{% set fullpath = env.doc2path(env.docname, base='tree/main/') %}
+{% set fullpath = env.doc2path(env.docname, base='tree/master/') %}
 
 .. only:: html
 
