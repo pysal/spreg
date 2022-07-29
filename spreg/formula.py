fd62497812f7fc7d7a2eae16a85f11eea7cd3fe7
thoffman1 <tylerhoff1@gmail.com>
2022-07-29 11:11:36 -0700

    add doctest to `formula.py`

--- spreg/formula.py
+++ spreg/formula.py
@@ -30,22 +30,97 @@ from .error_sp_het import GM_Combo_Het
 
 
 def noninplace_remove(lst, el):
+    """
+    Helper function to remove an element from a list and return the list.
+    """
+    
     lst.remove(el)
     return lst
 
 
 def expand_lag(w_name, fields):
     """
-    Creates a formula string with spatial lag terms for the given fields
+    Helper function for `from_formula`.
+    Creates a formula string with spatial lag terms for the given fields.
     """
+
     fields = [fields] if type(fields) == str else fields
     output = ["{" + w_name + ".sparse @ `" + field + "`}" for field in fields]
     return " + ".join(output)
 
 
 def from_formula(formula, df, w=None, method="gm", debug=False, **kwargs):
-    # Find all <...> and linearly lag every covar in there
-    # if "&" is found, switch to spatial error
+    """
+    Given a formula and a dataframe, parse the formula and return a configured
+    `spreg` model.
+ 
+    Syntax for the formulas is the same as `formulaic`, with two spatial operators added.
+    - The `<...>` operator:
+        - Enclose variables (covariates or response) in angle brackets to denote 
+          a spatial lag.
+        - `<` and `>` are not reserved characters in formulaic, so there are no conflicts.
+        - Usage: including `<FIELD>` in a formula adds the field and its spatial lag 
+          from the dataframe to the model matrix, unless `FIELD` is the y variable. In
+          that case, the code dispatches to the correct spatial lag estimation routine.
+        - Can use other transformations within `<...>`, 
+          e.g. `<{10*FIELD1} + FIELD2>` will be correctly parsed.
+    - The `&` symbol:
+        - Adds a spatial error component to a model.
+        - `&` is not a reserved character in formulaic, so there are no conflicts.
+    - The parser accepts combinations of `<...>` and `&`: 
+      `<FIELD1 + ... + FIELDN> + &` is the most general possible spatial model available. 
+   
+    Examples
+    --------
+
+    Import necessary libraries.
+
+    >>> import spreg
+    >>> import numpy as np
+    >>> import geopandas as gpd
+    >>> from libpysal.examples import load_example
+    >>> from libpysal.weights import Kernel, fill_diagonal
+    
+    Load the Boston housing example.
+
+    >>> boston = load_example("Bostonhsg")
+    >>> boston_df = gpd.read_file(boston.get_path("boston.shp"))
+    
+    Create a weights matrix and set its diagonal to zero (necessary for spatial
+    lag of y model).
+
+    >>> weights = Kernel(boston_df[["x", "y"]], k=50, fixed=False)
+    >>> weights = fill_diagonal(weights, 0)
+    
+    Write a formula. This formula incorporates transformations, a spatial lag of
+    a covariate, a spatial lag of the dependent variable, and a spatial error component.
+
+    >>> formula = "log(CMEDV) ~ {RM**2} + AGE + <log(CMEDV) + INDUS> + &"
+    
+    Fit model and output the formula as well.
+
+    >>> model, parsed_formula = spreg.from_formula(formula, boston_df, w=weights, debug=True)
+    >>> print(type(model))
+    <class 'spreg.error_sp.GM_Combo'>
+    >>> print(parsed_formula)
+    log(CMEDV) ~ {RM**2} + AGE + INDUS + {w.sparse @ `INDUS`}
+    >>> np.around(model.betas, decimals=4)
+    array([[ 2.4324e+00],
+        [-1.2000e-03],
+        [-0.0000e+00],
+        [ 2.4000e-02],
+        [-1.4000e-03],
+        [ 1.0000e-04],
+        [-8.1000e-03]])
+    >>> np.around(model.z_stat, decimals=6)
+    array([[ 2.0607149e+01,  0.0000000e+00],
+        [-1.7615890e+00,  7.8139000e-02],
+        [-1.2940000e-03,  9.9896700e-01],
+        [ 1.6212263e+01,  0.0000000e+00],
+        [-3.4082950e+00,  6.5400000e-04],
+        [ 8.4226000e-02,  9.3287700e-01]])
+    """
+
     lag_start_idx = formula.find("<")
     lag_end_idx = -1
     parsed_formula = formula[:lag_start_idx] if lag_start_idx >= 0 else ""
