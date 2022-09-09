70ac78a44b2cd377d1d6dd3f0f58b9bd0a59a374
thoffman1 <tylerhoff1@gmail.com>
2022-09-08 20:03:20 -0700

    finish docs and doctests for lag, durbin error, and durbin lag

--- spreg/sklearn/durbin.py
+++ spreg/sklearn/durbin.py
@@ -9,11 +9,77 @@ alternative uses of the Lag and Error classes.
 import numpy as np
 from .lag import Lag
 from .error import Error
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.extmath import safe_sparse_dot
+from scipy.stats import pearsonr
 
 
 class DurbinLag(Lag):
     """
-    Should inherit everything properly from Lag.
+    Syntactic sugar for fitting a spatial lag of X and spatial lag of Y model.
+    
+    Parameters
+    ----------
+    w            : libpysal.weights.W
+                   spatial weights object (always needed; default None)
+    fit_intercept: boolean
+                   when True, fits model with an intercept (default True)
+
+    Attributes
+    ----------
+    w            : libpysal.weights.W
+                   spatial weights object (always needed; default None)
+    coef_        : array
+                   kx1 array of estimated coefficients corresponding to direct effects
+    indir_coef_  : float
+                   scalar corresponding to indirect effect on the spatial lag term
+    intercept_   : float
+                   if fit_intercept is True, this is the intercept, otherwise undefined
+
+    Examples
+    --------
+
+    >>> import spreg.sklearn
+    >>> import numpy as np
+    >>> import geopandas as gpd
+    >>> from libpysal.examples import load_example
+    >>> from libpysal.weights import Kernel, fill_diagonal
+
+    Load data.
+
+    >>> boston = load_example("Bostonhsg")
+    >>> boston_df = gpd.read_file(boston.get_path("boston.shp"))
+
+    Transform variables prior to fitting regression.
+
+    >>> boston_df["RMSQ"] = boston_df["RM"]**2
+    >>> boston_df["LCMEDV"] = np.log(boston_df["CMEDV"])
+
+    Set up model matrices. We're going to predict log corrected median
+    house prices from the covariates.
+
+    >>> fields = ["RMSQ", "CRIM"]
+    >>> X = boston_df[fields].values
+    >>> y = boston_df["LCMEDV"].values
+
+    Create weights matrix.
+
+    >>> weights = Kernel(boston_df[["x", "y"]], k=50, fixed=False)
+    >>> weights = fill_diagonal(weights, 0)
+
+    Fit spatial lag model using default estimation method
+    (generalized method of moments).
+
+    >>> model = spreg.sklearn.DurbinLag(w=weights)
+    >>> model = model.fit(X, y)
+    >>> print(model.intercept_)
+    [1.71836332]
+    >>> print(model.coef_)
+    [[ 0.05990476 -0.02792598 -0.01464177  0.00336733]]
+    >>> print(model.indir_coef_)
+    [0.06211914]
+    >>> print(model.score(X, y))
+    0.042140182774405066
     """
 
     def fit(self, X, y, yend=None, q=None, w_lags=1, lag_q=True, method="gm", epsilon=1e-7):
@@ -25,22 +91,95 @@ class DurbinLag(Lag):
         X = np.hstack((X, self.w.full()[0] @ X))
         return super().fit(X, y, yend, q, w_lags, lag_q, method, epsilon)
 
-    def score(self, X, y, sample_weight=None):
+    def score(self, X, y):
         X = self._validate_data(X, accept_sparse=True)
 
         if self.w is None:
             raise ValueError("w must be libpysal.weights.W object")
 
         X = np.hstack((X, self.w.full()[0] @ X))
-        return super().score(X, y, sample_weight=sample_weight)
+        return super().score(X, y)
 
+    def _decision_function(self, X):
+        check_is_fitted(self)
+
+        return safe_sparse_dot(
+            np.linalg.inv(np.eye(self.w.n) - self.indir_coef_ * self.w.full()[0]),
+            safe_sparse_dot(X, self.coef_.T, dense_output=True), dense_output=True)
 
 class DurbinError(Error):
     """
-    Should inherit everything properly from Error.
+    Syntactic sugar for fitting a spatial lag of X and spatial error model.
+    
+    Parameters
+    ----------
+    w            : libpysal.weights.W
+                   spatial weights object (always needed; default None)
+    fit_intercept: boolean
+                   when True, fits model with an intercept (default True)
+
+    Attributes
+    ----------
+    w            : libpysal.weights.W
+                   spatial weights object (always needed; default None)
+    coef_        : array
+                   kx1 array of estimated coefficients corresponding to direct effects
+    indir_coef_  : float
+                   scalar corresponding to indirect effect on the spatial lag term
+    intercept_   : float
+                   if fit_intercept is True, this is the intercept, otherwise undefined
+
+    Examples
+    --------
+
+    >>> import spreg.sklearn
+    >>> import numpy as np
+    >>> import geopandas as gpd
+    >>> from libpysal.examples import load_example
+    >>> from libpysal.weights import Kernel, fill_diagonal
+
+    Load data.
+
+    >>> boston = load_example("Bostonhsg")
+    >>> boston_df = gpd.read_file(boston.get_path("boston.shp"))
+
+    Transform variables prior to fitting regression.
+
+    >>> boston_df["RMSQ"] = boston_df["RM"]**2
+    >>> boston_df["LCMEDV"] = np.log(boston_df["CMEDV"])
+
+    Set up model matrices. We're going to predict log corrected median
+    house prices from the covariates.
+
+    >>> fields = ["RMSQ", "CRIM"]
+    >>> X = boston_df[fields].values
+    >>> y = boston_df["LCMEDV"].values
+
+    Create weights matrix.
+
+    >>> weights = Kernel(boston_df[["x", "y"]], k=50, fixed=False)
+    >>> weights = fill_diagonal(weights, 0)
+
+    Fit spatial error model using default estimation method
+    (generalized method of moments).
+
+    >>> model = spreg.sklearn.DurbinError(w=weights)
+    >>> model = model.fit(X, y)
+    >>> print(model.intercept_)
+    [2.43723301]
+    >>> print(model.coef_)
+    [[ 0.02694104 -0.00739781 -0.00053044 -0.00160177]]
+    >>> print(model.indir_coef_)
+    -0.046479712483854504
+    >>> print(model.score(X, y))
+    0.6541881277608026
     """
 
     def fit(self, X, y, yend=None, q=None, method="gm", epsilon=1e-7):
+        """
+        Fit Durbin Error model.
+        """
+        
         X = self._validate_data(X, accept_sparse=True)
 
         if self.w is None:
@@ -50,47 +189,28 @@ class DurbinError(Error):
         return super().fit(X, y, yend, q, method, epsilon)
 
     def score(self, X, y, sample_weight=None):
+        """
+        Return R2 value for Durbin Error model.
+        """
+
         X = self._validate_data(X, accept_sparse=True)
 
         if self.w is None:
             raise ValueError("w must be libpysal.weights.W object")
 
         X = np.hstack((X, self.w.full()[0] @ X))
-        return super().score(X, y, sample_weight=sample_weight)
+        return super().score(X, y)
 
+    def score(self, X, y):
+        """
+        Computes pseudo R2 for the spatial error model.
+        """
 
-if __name__ == "__main__":
-    import spreg
-    import numpy as np
-    import geopandas as gpd
-    from libpysal.examples import load_example
-    from libpysal.weights import Kernel, fill_diagonal
+        y_pred = self.predict(X)
+        return float(pearsonr(y.flatten(), y_pred.flatten())[0]**2)
 
-    boston = load_example("Bostonhsg")
-    boston_df = gpd.read_file(boston.get_path("boston.shp"))
+    def _decision_function(self, X):
+        check_is_fitted(self)
 
-    boston_df["NOXSQ"] = (10 * boston_df["NOX"])**2
-    boston_df["RMSQ"] = boston_df["RM"]**2
-    boston_df["LOGDIS"] = np.log(boston_df["DIS"].values)
-    boston_df["LOGRAD"] = np.log(boston_df["RAD"].values)
-    boston_df["TRANSB"] = boston_df["B"].values / 1000
-    boston_df["LOGSTAT"] = np.log(boston_df["LSTAT"].values)
-
-    fields = ["RMSQ", "CRIM"]
-    X = boston_df[fields].values
-    y = np.log(boston_df["CMEDV"].values)  # predict log corrected median house prices from covars
-
-    weights = Kernel(boston_df[["x", "y"]], k=50, fixed=False)
-    weights = fill_diagonal(weights, 0)
-    
-    model = spreg.DurbinLag(w=weights)
-    model.fit(X, y)
-    print(model.intercept_)
-    print(model.coef_)
-    print(model.indir_coef_)
-
-    model = spreg.DurbinError(w=weights)
-    model.fit(X, y)
-    print(model.intercept_)
-    print(model.coef_)
-    print(model.indir_coef_)
+        X = np.hstack((X, self.w.full()[0] @ X))
+        return safe_sparse_dot(X, self.coef_.T, dense_output=True) + self.intercept_
--- spreg/sklearn/lag.py
+++ spreg/sklearn/lag.py
@@ -16,9 +16,89 @@ from sklearn.utils.validation import check_is_fitted
 from sklearn.linear_model._base import LinearModel
 from libpysal.weights import lag_spatial
 from scipy.optimize import minimize_scalar
+from scipy.stats import pearsonr
 
 
 class Lag(RegressorMixin, LinearModel):
+    """
+    Unified scikit-learn style estimator for spatial lag models
+
+    Parameters
+    ----------
+    w            : libpysal.weights.W
+                   spatial weights object (always needed; default None)
+    fit_intercept: boolean
+                   when True, fits model with an intercept (default True)
+
+    Attributes
+    ----------
+    w            : libpysal.weights.W
+                   spatial weights object (always needed; default None)
+    coef_        : array
+                   kx1 array of estimated coefficients corresponding to direct effects
+    indir_coef_  : float
+                   scalar corresponding to indirect effect on the spatial lag term
+    intercept_   : float
+                   if fit_intercept is True, this is the intercept, otherwise undefined
+
+    Examples
+    --------
+
+    >>> import spreg.sklearn
+    >>> import numpy as np
+    >>> import geopandas as gpd
+    >>> from libpysal.examples import load_example
+    >>> from libpysal.weights import Kernel, fill_diagonal
+
+    Load data.
+
+    >>> boston = load_example("Bostonhsg")
+    >>> boston_df = gpd.read_file(boston.get_path("boston.shp"))
+
+    Transform variables prior to fitting regression.
+
+    >>> boston_df["RMSQ"] = boston_df["RM"]**2
+    >>> boston_df["LCMEDV"] = np.log(boston_df["CMEDV"])
+
+    Set up model matrices. We're going to predict log corrected median
+    house prices from the covariates.
+
+    >>> fields = ["RMSQ", "CRIM"]
+    >>> X = boston_df[fields].values
+    >>> y = boston_df["LCMEDV"].values
+
+    Create weights matrix.
+
+    >>> weights = Kernel(boston_df[["x", "y"]], k=50, fixed=False)
+    >>> weights = fill_diagonal(weights, 0)
+
+    Fit spatial lag model using default estimation method
+    (generalized method of moments).
+
+    >>> model = spreg.sklearn.Lag(w=weights)
+    >>> model = model.fit(X, y)
+    >>> print(model.intercept_)
+    [2.23457809]
+    >>> print(model.coef_)
+    [[ 0.02486769 -0.02033653]]
+    >>> print(model.indir_coef_)
+    [-0.00243977]
+    >>> print(model.score(X, y))
+    0.5773206900447707
+
+    Fit spatial error model using default estimation method (maximum
+    likelihood).
+
+    >>> model.fit(X, y, method="full")
+    >>> print(model.intercept_)
+    [2.15295169]
+    >>> print(model.coef_)
+    [[ 0.02500276 -0.01998133]]
+    >>> print(model.indir_coef_)
+    -0.000928393589087051
+    >>> print(model.score(X, y))
+    0.5786266229308156
+    """
     def __init__(self, w=None, fit_intercept=True):
         self.w = w
         self.fit_intercept = fit_intercept
@@ -32,6 +112,40 @@ class Lag(RegressorMixin, LinearModel):
             safe_sparse_dot(X, self.coef_.T, dense_output=True), dense_output=True)
 
     def fit(self, X, y, yend=None, q=None, w_lags=1, lag_q=True, method="gm", epsilon=1e-7):
+        """
+        Fit spatial lag model.
+
+        Parameters
+        ----------
+        X               : array
+                          nxk array of covariates
+        y               : array
+                          nx1 array of dependent variable
+        yend            : array
+                          nxp array of endogenous variables (default None)
+        q               : array
+                          nxp array of external endogenous variables to use as instruments
+                          (should not contain any variables in X; default None)
+        w_lags          : integer
+                          orders of W to include as instruments for the spatially 
+                          lagged dependent variable. For example, w_lags=1, then 
+                          instruments are WX; if w_lags=2, then WX, WWX; and so on.
+                          (default 1)
+        method          : string
+                          name of estimation method to use (default "gm"). available options
+                          are: "gm" (generalized method of moments), "full" (brute force 
+                          maximum likelihood), "lu" (LU sparse decomposition maximum 
+                          likelihood), and "ord" (Ord eigenvalue method)
+        epsilon         : float
+                          tolerance to use for fitting maximum likelihood models
+                          (default 1e-7)
+        
+        Returns
+        -------
+        self            : Lag 
+                          fitted spreg.sklearn.Lag object
+        """
+
         # Input validation
         X, y = self._validate_data(X, y, accept_sparse=True, y_numeric=True)
         y = y.reshape(-1, 1)  # ensure vector TODO FORMALIZE THIS
@@ -53,6 +167,10 @@ class Lag(RegressorMixin, LinearModel):
         return self
 
     def _fit_gm(self, X, y, yend, q, w_lags, lag_q):
+        """
+        Helper method for fitting with GMM
+        """
+
         yend2, q2 = set_endog(y, X[:, 1:], self.w,
                               yend, q, w_lags, lag_q)  # assumes constant in first column
 
@@ -80,6 +198,10 @@ class Lag(RegressorMixin, LinearModel):
         self.indir_coef_ = params_[-1]
     
     def _fit_ml(self, X, y, method, epsilon):
+        """
+        Helper method for fitting with maximum likelihood
+        """
+
         ylag = lag_spatial(self.w, y)
         xtx = spdot(X.T, X)
         xtxi = np.linalg.inv(xtx)
@@ -115,6 +237,10 @@ class Lag(RegressorMixin, LinearModel):
             self.coef_ = params_.T
 
     def _log_likelihood(self, rho, e0, e1, evals, method):
+        """
+        Defines log likelihoods for each of the three maximum likelihood methods
+        """
+
         n = self.w.n
         er = e0 - rho * e1
         sig2 = spdot(er.T, er) / n
@@ -139,65 +265,10 @@ class Lag(RegressorMixin, LinearModel):
         clik = nlsig2 - jacob
         return clik
 
+    def score(self, X, y):
+        """
+        Computes pseudo R2 for the spatial lag model.
+        """
 
-if __name__ == "__main__":
-    import spreg
-    import numpy as np
-    import pandas as pd
-    import geopandas as gpd
-    from libpysal.examples import load_example
-    from libpysal.weights import Kernel, fill_diagonal
-
-    boston = load_example("Bostonhsg")
-    boston_df = gpd.read_file(boston.get_path("boston.shp"))
-
-    boston_df["NOXSQ"] = (10 * boston_df["NOX"])**2
-    boston_df["RMSQ"] = boston_df["RM"]**2
-    boston_df["LOGDIS"] = np.log(boston_df["DIS"].values)
-    boston_df["LOGRAD"] = np.log(boston_df["RAD"].values)
-    boston_df["TRANSB"] = boston_df["B"].values / 1000
-    boston_df["LOGSTAT"] = np.log(boston_df["LSTAT"].values)
-
-    fields = ["RMSQ", "CRIM"]
-    X = boston_df[fields].values
-    y = np.log(boston_df["CMEDV"].values)  # predict log corrected median house prices from covars
-
-    weights = Kernel(boston_df[["x", "y"]], k=50, fixed=False)
-    weights = fill_diagonal(weights, 0)
-
-    model = spreg.Lag(w=weights)
-    model.fit(X, y)
-    print(model.intercept_)
-    print(model.coef_)
-    print(model.indir_coef_)
-    print(model.score(X, y))
-
-    old_model = spreg.GM_Lag(y, X, w=weights)
-    print(old_model.betas)
-
-    model.fit(X, y, method="full")
-    print(model.intercept_)
-    print(model.coef_)
-    print(model.indir_coef_)
-    print(model.score(X, y))
-
-    old_model = spreg.ML_Lag(y, X, w=weights, method="full")
-    print(old_model.betas)
-
-    model.fit(X, y, method="lu")
-    print(model.intercept_)
-    print(model.coef_)
-    print(model.indir_coef_)
-    print(model.score(X, y))
-
-    old_model = spreg.ML_Lag(y, X, w=weights, method="lu")
-    print(old_model.betas)
-
-    model.fit(X, y, method="ord")
-    print(model.intercept_)
-    print(model.coef_)
-    print(model.indir_coef_)
-    print(model.score(X, y))
-
-    old_model = spreg.ML_Lag(y, X, w=weights, method="ord")
-    print(old_model.betas)
+        y_pred = self.predict(X)
+        return float(pearsonr(y.flatten(), y_pred.flatten())[0]**2)
