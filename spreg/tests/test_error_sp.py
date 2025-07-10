a6ed8eec398026da8b08bb944e136956fb47ac00
Maria Alice <mariaalice_souza@outlook.com>
2025-07-10 13:34:45 -0300

    increase-coverage
    enabling previously deleted tests in test_panel_re; add slx_lags > 0 in test_error_sp_regimes;
    add slx_lags > 0 in GM_Comb and test_model regimes in GMM_Error

--- spreg/tests/test_error_sp.py
+++ spreg/tests/test_error_sp.py
@@ -6,6 +6,8 @@ import spreg
 from spreg import error_sp as SP
 from libpysal.common import RTOL
 import pandas as pd
+from libpysal.examples import load_example
+from spreg import GMM_Error_Regimes
 
 class TestBaseGMError(unittest.TestCase):
     def setUp(self):
@@ -208,6 +210,41 @@ class TestGMEndogError(unittest.TestCase):
         z_stat = np.array([[ 2.40664208,  0.01609994], [ 0.63144305,  0.52775088], [-1.75659016,  0.07898769]])
         np.testing.assert_allclose(reg.z_stat,z_stat,RTOL)
 
+    def test_model_1(self):
+        reg = SP.GM_Endog_Error(self.y, self.X, self.yd, self.q, self.w, slx_lags=1)
+        betas = np.array([[76.77491588], [0.21332977], [-0.62211115], [-0.91101459], [0.40667462]])
+        np.testing.assert_allclose(reg.betas,betas,RTOL +.0001)
+        print('Running reduced precision test in L205 of test_error_sp.py')
+        np.testing.assert_allclose(reg.betas,betas,RTOL +.0001)
+        u = np.array([25.419675300942714])
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
+        e_filtered = np.array([31.207183005033297])
+        np.testing.assert_allclose(reg.e_filtered[0], e_filtered, RTOL)
+        predy = np.array([55.04732769905729])
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
+        n = 49
+        np.testing.assert_allclose(reg.n,n,RTOL)
+        k = 4
+        np.testing.assert_allclose(reg.k,k,RTOL)
+        y = np.array([80.467003])
+        np.testing.assert_allclose(reg.y[0],y,RTOL)
+        x = np.array([1.0, 19.531, 18.594])
+        np.testing.assert_allclose(reg.x[0],x,RTOL)
+        yend = np.array([15.72598])
+        np.testing.assert_allclose(reg.yend[0],yend,RTOL)
+        z = np.array([1.0, 19.531, 18.594, 15.72598])
+        np.testing.assert_allclose(reg.z[0],z,RTOL)
+        mean_y = 38.43622446938775
+        np.testing.assert_allclose(reg.mean_y,mean_y,RTOL)
+        std_y = 18.466069465206047
+        np.testing.assert_allclose(reg.std_y,std_y,RTOL)
+        vm = np.array( [[1241.3679785142479, -25.45186633997482, -21.57345029999624, -15.508445428953587],
+                         [-25.451866339974888, 0.6839492993049352, 0.2740158770507004, 0.3315209826545654], 
+                         [-21.573450299996, 0.27401587705069513, 0.6877711470567254, 0.20979319854002507], 
+                         [-15.508445428953618, 0.3315209826545651, 0.2097931985400285, 0.2178378881170716]] )
+        np.testing.assert_allclose(reg.vm,vm,RTOL)
+        sig2 = 203.39160138851173
+        np.testing.assert_allclose(reg.sig2,sig2,RTOL)     
 
 class TestGMCombo(unittest.TestCase):
     def setUp(self):
@@ -220,6 +257,7 @@ class TestGMCombo(unittest.TestCase):
         self.X = np.array(X).T
         self.w = libpysal.weights.Rook.from_shapefile(libpysal.examples.get_path("columbus.shp"))
         self.w.transform = 'r'
+
     def test_model(self):
         # Only spatial lag
         reg = SP.GM_Combo(self.y, self.X, w=self.w)
@@ -264,6 +302,86 @@ class TestGMCombo(unittest.TestCase):
         np.testing.assert_allclose(reg.std_err,std_err,RTOL)
         z_stat = np.array([[  2.52051597e+00,   1.17182922e-02], [  1.50535954e+00,   1.32231664e-01], [ -3.31909311e+00,   9.03103123e-04], [ -4.68530506e-01,   6.39405261e-01]])
         np.testing.assert_allclose(reg.z_stat,z_stat,RTOL)
+
+    def test_model_1(self):
+        # SLX > 0, slx_vars = "All" and  name_x=["income", "crime"]
+        reg = SP.GM_Combo(self.y, self.X, w=self.w,slx_lags=1, slx_vars="All", name_x=["income", "crime"])
+        betas = np.array([[-1.68549776], [1.023858], [-0.60027516], [-1.24056691], [0.58796177], [1.1429901], [-0.64301075]])
+        np.testing.assert_allclose(reg.betas,betas,RTOL+.0001)
+        u = np.array([39.60278686988822])
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
+        e_filtered = np.array([27.387967128789217])
+        np.testing.assert_allclose(reg.e_filtered[0], e_filtered, RTOL)
+        predy = np.array([40.864216130111785])
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
+        n = 49
+        np.testing.assert_allclose(reg.n,n,RTOL)
+        k = 6
+        np.testing.assert_allclose(reg.k,k,RTOL)
+        y = np.array([80.467003])
+        np.testing.assert_allclose(reg.y[0],y,RTOL)
+        x = np.array([1.0, 19.531, 15.72598, 18.594, 24.7142675])
+        np.testing.assert_allclose(reg.x[0],x,RTOL)
+        yend = np.array([35.4585005])
+        np.testing.assert_allclose(reg.yend[0],yend,RTOL)
+        z = np.array([1.0, 19.531, 15.72598, 18.594, 24.7142675, 35.4585005])
+        np.testing.assert_allclose(reg.z[0],z,RTOL)
+        mean_y = 38.43622446938775
+        np.testing.assert_allclose(reg.mean_y,mean_y,RTOL)
+        std_y = 18.466069465206047
+        np.testing.assert_allclose(reg.std_y,std_y,RTOL)
+        vm = np.array( [[310.6283901851071, -1.8625762732506532, 0.23637750381058148, 
+                         -2.490257967992913, -3.540727963871908, -3.3853342129346014],
+                           [-1.862576273250409, 0.25454991581277797, 0.04775611402222607,
+                             -0.3159417698794814, -0.0361064859627287, 0.0622148895979572], 
+                             [0.23637750381059774, 0.04775611402223329, 0.0348942850522157,
+                               -0.08708418052902171, -0.04146284919474196, 0.01503172670815147],
+                                 [-2.4902579679940353, -0.31594176987947975, -0.08708418052899158,
+                                   1.1272379141717317, 0.17905841078689427, -0.32987287666815707],
+                                     [-3.5407279638721842, -0.03610648596273606, -0.04146284919474014,
+                                       0.1790584107869242, 0.0916798380512302, -0.00838180184803425], 
+                                       [-3.3853342129346173, 0.062214889597958206, 0.015031726708141036,
+                                         -0.32987287666815596, -0.00838180184802332, 0.18434843597847317]])
+        np.testing.assert_allclose(reg.vm,vm,RTOL)
+        sig2 = 136.12652143339153
+        np.testing.assert_allclose(reg.sig2,sig2,RTOL)
+
+    def test_model_2(self):
+        # SLX > 0 and slx_vars = [True, False]
+        reg = SP.GM_Combo(self.y, self.X, w=self.w,slx_lags=1, slx_vars=[True, False])
+        betas = np.array([[2.741563], [1.419321], [-0.378284], [-4.731508], [2.544966], [-0.491424]])
+        np.testing.assert_allclose(reg.betas,betas,RTOL+.0001)
+        u = np.array([53.69055409811362])
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
+        e_filtered = np.array([35.74670887492897])
+        np.testing.assert_allclose(reg.e_filtered[0], e_filtered, RTOL)
+        predy = np.array([26.776448901886383])
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
+        n = 49
+        np.testing.assert_allclose(reg.n,n,RTOL)
+        k = 5
+        np.testing.assert_allclose(reg.k,k,RTOL)
+        y = np.array([80.467003])
+        np.testing.assert_allclose(reg.y[0],y,RTOL)
+        x = np.array([1.0, 19.531, 15.72598, 18.594])
+        np.testing.assert_allclose(reg.x[0],x,RTOL)
+        yend =yend = np.array([35.4585005])
+        np.testing.assert_allclose(reg.yend[0],yend,RTOL)
+        np.array([1.0, 19.531, 15.72598, 18.594, 35.4585005])
+        z = np.array([1.0, 19.531, 15.72598, 18.594, 35.4585005])
+        np.testing.assert_allclose(reg.z[0],z,RTOL)
+        mean_y = 38.43622446938775
+        np.testing.assert_allclose(reg.mean_y,mean_y,RTOL)
+        std_y = 18.466069465206047
+        np.testing.assert_allclose(reg.std_y,std_y,RTOL)
+        vm = np.array( [[755.7953159663382, -12.278439962044509, -3.6459728883651974, 43.202043635631924, -28.097821060591997],
+                         [-12.278439962045278, 0.633225888520698, 0.08271240441724638, -0.9873776278245777, 0.38198144440688786],
+                           [-3.6459728883659617, 0.08271240441725672, 0.04448974215545745, -0.0020962177642369043, 0.02383861772648586], 
+                           [43.20204363563919, -0.9873776278246708, -0.0020962177641989273, 5.682136109327099, -2.917681968518343],
+                             [-28.097821060596054, 0.381981444406925, 0.023838617726460676, -2.917681968518344, 1.6782119652110892]] )
+        np.testing.assert_allclose(reg.vm,vm,RTOL)
+        sig2 = 349.6934929722473
+        np.testing.assert_allclose(reg.sig2,sig2,RTOL)
 
 class TestGMMError(unittest.TestCase):
     def setUp(self):
@@ -414,64 +532,92 @@ class TestGMMError(unittest.TestCase):
                                0.00000000e+00,  0.00000000e+00,  2.67800026e-02]])
         np.testing.assert_allclose(reg.vm,vm,RTOL)
 
-        reg = SP.GMM_Error(self.db[["HOVAL"]], self.db[["INC"]],
-                            yend=self.db[["CRIME"]], 
-                            q=self.db[["DISCBD"]], 
-                            w=self.w, slx_lags=1) 
-        betas = np.array([ 
-            [76.89629412], 
-            [ 0.21526617],
-            [-0.61826513],
-            [-0.9       ], 
-            [ 0.43003216]])
-        np.testing.assert_allclose(reg.betas,betas,RTOL)
-        vm = np.array([
-            [912.63997789, -19.62464020, -7.03524948, -13.72433338, 1.84512875],
-            [-19.62464020, 0.52955014, -0.00617855, 0.31294813, -0.04530781],
-            [-7.03524948, -0.00617855, 0.37552517, 0.05701885, 0.00382282],
-            [-13.72433338, 0.31294813, 0.05701885, 0.22015356, -0.03136501],
-            [1.84512875, -0.04530781, 0.00382282, -0.03136501, 0.03001728]])
-        np.testing.assert_allclose(reg.vm,vm,RTOL)
-
-        reg = SP.GMM_Error(self.db[["HOVAL"]], self.db[["INC", "CRIME"]],
-                                   slx_vars="All", w=self.w, slx_lags=1) 
+    def setUp_regimes(self): 
+        nat = load_example('Natregimes')
+        db = libpysal.io.open(nat.get_path("natregimes.dbf"),'r')
+        self.y_var = 'HR90'
+        self.y = np.array([db.by_col(self.y_var)]).reshape(3085,1)
+        self.x_var = ['PS90','UE90']
+        self.x = np.array([db.by_col(name) for name in self.x_var]).T
+        self.r_var = 'SOUTH'
+        self.regimes = db.by_col(self.r_var)
+        self.w = libpysal.weights.Rook.from_shapefile(nat.get_path("natregimes.shp"))
+        self.w.transform = 'r' 
+        
+    def test_regimes(self):
+        self.setUp_regimes()
+        reg = GMM_Error_Regimes(self.y, self.x, self.regimes, self.w)
         betas = np.array([
-            [29.38238574],
-            [ 0.82921502],
-            [-0.57499819],
-            [ 0.48748671],
-            [ 0.29556428],
-            [ 0.39636619]])
-        np.testing.assert_allclose(reg.betas,betas,RTOL)
+        [0.068987],
+        [0.788486], 
+        [0.539782], 
+        [5.094800],  
+        [1.196472],  
+        [0.601786],  
+        [0.427196]  ])
+        np.testing.assert_allclose(reg.betas,betas,rtol=1e-4, atol=1e-5)
         vm = np.array([
-            [5.97495766e+02, -5.55463269e+00, -6.81492201e-01, -1.53802421e+01, -7.21116007e+00, 0.00000000e+00],
-            [-5.55463269e+00, 1.34368489e-01, 2.10846256e-02, 3.83654841e-02, 5.54102390e-02, 0.00000000e+00],
-            [-6.81492201e-01, 2.10846256e-02, 2.56742519e-02, 2.40939941e-03, -1.84082192e-02, 0.00000000e+00],
-            [-1.53802421e+01, 3.83654841e-02, 2.40939941e-03, 6.69703341e-01, 1.58284990e-01, 0.00000000e+00],
-            [-7.21116007e+00, 5.54102390e-02, -1.84082192e-02, 1.58284990e-01, 1.30057684e-01, 0.00000000e+00],
-            [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 2.67800026e-02]
-            ])
-        np.testing.assert_allclose(reg.vm,vm,RTOL)
+        [1.26869e-01, -2.58360e-02, -1.86910e-02, 3.49500e-03, -9.33000e-04, -4.78000e-04, 0.00000e+00],
+        [-2.58360e-02, 4.91320e-02, 2.88600e-03, -8.58000e-04, 1.86800e-03, 1.53000e-04, 0.00000e+00],
+        [-1.86910e-02, 2.88600e-03, 3.53200e-03, -1.59000e-04, 2.13000e-04, 6.40000e-05, 0.00000e+00],
+        [3.49500e-03, -8.58000e-04, -1.59000e-04, 4.70141e-01, -9.18920e-02, -5.77590e-02, 0.00000e+00],
+        [-9.33000e-04, 1.86800e-03, 2.13000e-04, -9.18920e-02, 1.28565e-01, 1.05540e-02, 0.00000e+00],
+        [-4.78000e-04, 1.53000e-04, 6.40000e-05, -5.77590e-02, 1.05540e-02, 8.58300e-03, 0.00000e+00],
+        [0.00000e+00, 0.00000e+00, 0.00000e+00, 0.00000e+00, 0.00000e+00, 0.00000e+00, 6.37000e-04]])
+        np.testing.assert_allclose(reg.vm,vm,rtol=1e-4, atol=1e-5)
 
-        reg = SP.GMM_Error(self.db[["HOVAL"]], self.db[["INC", "CRIME"]],
-                       slx_vars=[True, True], w=self.w, slx_lags=1) 
-        betas = np.array([
-            [29.38238574],
-            [ 0.82921502],
-            [-0.57499819],
-            [ 0.48748671],
-            [ 0.29556428],
-            [ 0.39636619]])
-        np.testing.assert_allclose(reg.betas,betas,RTOL)
-        vm = np.array([
-            [5.97495766e+02, -5.55463269e+00, -6.81492201e-01, -1.53802421e+01, -7.21116007e+00, 0.00000000e+00],
-              [-5.55463269e+00, 1.34368489e-01, 2.10846256e-02, 3.83654841e-02, 5.54102390e-02, 0.00000000e+00],
-              [-6.81492201e-01, 2.10846256e-02, 2.56742519e-02, 2.40939941e-03, -1.84082192e-02, 0.00000000e+00],
-              [-1.53802421e+01, 3.83654841e-02, 2.40939941e-03, 6.69703341e-01, 1.58284990e-01, 0.00000000e+00],
-              [-7.21116007e+00, 5.54102390e-02, -1.84082192e-02, 1.58284990e-01, 1.30057684e-01, 0.00000000e+00],
-              [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 2.67800026e-02]
-              ])
-        np.testing.assert_allclose(reg.vm,vm,RTOL)
+class TestGMComboExample(unittest.TestCase):
+    def setUp(self):
+        self.db = libpysal.io.open(libpysal.examples.get_path('columbus.dbf'), 'r')
+        self.y = np.array(self.db.by_col("HOVAL"))
+        self.y = np.reshape(self.y, (49,1))
+        self.X = np.array([self.db.by_col("INC")]).T
+        self.yd = np.array([self.db.by_col("CRIME")]).T
+        self.q = np.array([self.db.by_col("DISCBD")]).T
+        self.w = libpysal.weights.Rook.from_shapefile(libpysal.examples.get_path("columbus.shp"))
+        self.w.transform = 'r'
+
+    def test_model(self):
+        import io
+        import sys
+        from contextlib import redirect_stdout
+        f = io.StringIO()
+        with redirect_stdout(f):
+            reg = SP.GM_Combo(self.y, self.X, self.yd, self.q, w=self.w, 
+                       name_x=['inc'], name_y='hoval', name_yend=['crime'], name_q=['discbd'],
+                       name_ds='columbus', vm=True)
+            betas = np.array([[111.90452543], [-0.31201292], [-1.36030107], [-0.5319876], [0.68161358]])
+            np.testing.assert_allclose(reg.betas,betas,RTOL+.0001)
+            u = np.array([14.911951654904712])
+            np.testing.assert_allclose(reg.u[0],u,RTOL)
+            e_filtered = np.array([23.46232931535747])
+            np.testing.assert_allclose(reg.e_filtered[0], e_filtered, RTOL)
+            predy = np.array([65.5550513450953])
+            np.testing.assert_allclose(reg.predy[0],predy,RTOL)
+            n = 49
+            np.testing.assert_allclose(reg.n,n,RTOL)
+            k = 4
+            np.testing.assert_allclose(reg.k,k,RTOL)
+            y = np.array([80.467003])
+            np.testing.assert_allclose(reg.y[0],y,RTOL)
+            x = np.array([1.0, 19.531])
+            np.testing.assert_allclose(reg.x[0],x,RTOL)
+            yend = np.array([15.72598, 35.4585005])
+            np.testing.assert_allclose(reg.yend[0],yend,RTOL)
+            z = np.array([1.0, 19.531, 15.72598, 35.4585005 ])
+            np.testing.assert_allclose(reg.z[0],z,RTOL)
+            mean_y = 38.43622446938775
+            np.testing.assert_allclose(reg.mean_y,mean_y,RTOL)
+            std_y = 18.466069465206047
+            np.testing.assert_allclose(reg.std_y,std_y,RTOL)
+            vm = np.array( [[5016.050352148409, -76.53478080595559, -44.251807673048404, -59.28554254165969],
+                         [-76.53478080595606, 1.4067533165464567, 0.7448544825341058, 0.7791558312477468],
+                           [-44.25180767304751, 0.7448544825340884, 0.48156504963672947, 0.42796705603821317], 
+                           [-59.285542541663446, 0.779155831247796, 0.427967056038256, 0.8430846436101599]] )
+            np.testing.assert_allclose(reg.vm,vm,RTOL)
+            print(reg.output)
+            print(reg.summary)
+        output = f.getvalue()
 
 if __name__ == '__main__':
     unittest.main()
